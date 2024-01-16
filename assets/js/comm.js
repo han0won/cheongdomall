e65907d700f35a37c91c1b17bd5fa301aa9fe3f6
UDintheroom <dkaqktk38@gmail.com>
2024-01-16 17:01:50 +0900

    main swiper slide 수정 중

---
+++ assets/js/comm.js
@@ -0,0 +1,13 @@
+// main: 신상품
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
