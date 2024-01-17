55ba639743344439e0c95ad962d32ffb405d1844
UDintheroom <dkaqktk38@gmail.com>
2024-01-17 15:22:59 +0900

    신상품 slide 수정 중

--- assets/js/comm.js
+++ assets/js/comm.js
@@ -1,13 +1,22 @@
 // main: 신상품
-var swiper = new Swiper(".new-slide", {
-  slidesPerView: 3,
-  spaceBetween: 30,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
+// var swiper = new Swiper(".new-slide", {
+//   slidesPerView: 3,
+//   spaceBetween: 20,
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+// });
+
+$(document).ready(function() {
+  $('.new-item-card-wrap').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
 });
