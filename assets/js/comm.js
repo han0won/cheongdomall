86fc10cd2636b745ad708c7efc52c7ba9b150824
UDintheroom <dkaqktk38@gmail.com>
2024-01-17 18:18:44 +0900

    불필요한 image 삭제, 신상품 slide(slick) 설정

--- assets/js/comm.js
+++ assets/js/comm.js
@@ -1,22 +1,10 @@
 // main: 신상품
-// var swiper = new Swiper(".new-slide", {
-//   slidesPerView: 3,
-//   spaceBetween: 20,
-//   navigation: {
-//     nextEl: ".swiper-button-next",
-//     prevEl: ".swiper-button-prev",
-//   },
-//   pagination: {
-//     el: ".swiper-pagination",
-//     clickable: true,
-//   },
-// });
-
 $(document).ready(function() {
-  $('.new-item-card-wrap').slick({
-    slidesToShow: 3,
+  $('.new-card-wrap').slick({
+    slidesToShow: 4,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 2000,
+    autoplaySpeed: 3000,
   });
+
 });
