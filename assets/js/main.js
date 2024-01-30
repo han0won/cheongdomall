e766be132d85e481ed30900ee2a7fc91c84caabe
UDintheroom <dkaqktk38@gmail.com>
2024-01-30 18:21:03 +0900

    all slides edit

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,63 +3,54 @@ $(document).ready(function() {
   // main banner
   $('.slide-wrap').slick({
     dots: true,
-    // autoplay: true,
-    // autoplaySpeed: 3500,
+    autoplay: true,
+    autoplaySpeed: 3500,
     centerMode: true,
     variableWidth: true,
   });
 
   // new items
   $('.new-card-wrap').slick({
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
     slidesToShow: 4,
     slidesToScroll: 1,
-    prevArrow: $('.new-prev'),
-    nextArrow: $('.new-next'),
-    // autoplay: true,
-    autoplaySpeed: 3000,
   });
 
+// 
 
 //  best tabs, swiper
-    // 탭 버튼과 탭 컨텐츠를 선택
     const tabButtons = document.querySelectorAll('.tab-btn')
     const tabs = document.querySelectorAll('.best-tab')
-    // 모든 슬라이더에 대한 Swiper 인스턴스를 저장할 배열
     let swipers = []
-    // Swiper 인스턴스를 초기화하는 함수
     function initSwiper(swiperElement) {
         return new Swiper(swiperElement, {
             // autoplay: true,
             loop: true,
+            loopAdditionalSlides : 1,
             slidesPerView: 5,
             centeredSlides: true,
-            navigation: {
-                nextEl: '.swiper-button-next',
-                prevEl: '.swiper-button-prev',
+            scrollbar: {
+              el: ".best-scrollbar",
+              clickable: true,
             },
         })
     }
-    // 탭을 변경하는 함수
     function changeTab(index) {
-        // 모든 탭과 버튼에서 'active' 클래스 제거
         tabButtons.forEach((button) => button.classList.remove('active'))
         tabs.forEach((tab) => tab.classList.remove('active'))
-        // 선택된 탭과 버튼에 'active' 클래스 추가
         tabButtons[index].classList.add('active')
         tabs[index].classList.add('active')
-        // 해당 탭에 Swiper 인스턴스가 없으면 초기화
         if (!swipers[index]) {
             const swiperElement = tabs[index].querySelector('.swiper')
             swipers[index] = initSwiper(swiperElement)
         }
     }
-    // 탭 버튼에 클릭 이벤트 리스너 추가
     tabButtons.forEach((button, index) => {
         button.addEventListener('click', () => changeTab(index))
     })
-    // 초기 탭 설정
-    changeTab(0) // 첫 번째 탭을 기본값으로 설정
-
+    changeTab(0)
 
   // recommend
   var swiper = new Swiper(".reco-slide", {
@@ -67,11 +58,10 @@ $(document).ready(function() {
     slidesPerView: "auto",
     spaceBetween: 40,
     pagination: {
-      el: ".swiper-pagination",
+      el: ".reco-pagination",
       clickable: true,
     },
   });
-
 
   // farm story tabs
   $(document).ready(function() {
@@ -81,14 +71,11 @@ $(document).ready(function() {
   $('.tab-btn').click(function(){
     var $this = $(this);
     var index = $this.index();
-    
     $this.addClass('active');
     $this.siblings('.tab-btn.active').removeClass('active');
-    
     var $outer = $this.closest('.farm');
     var $current = $outer.find('.farm-tabs .farm-tab.active');
     var $post = $outer.find('.farm-tabs .farm-tab').eq(index);
-    
     $current.removeClass('active');
     $post.addClass('active');
     });
